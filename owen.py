36a3b038c6620508e021be4882d090039c9c8dc6
jamieRollison <rollisonjamie15@gmail.com>
2022-03-03 18:21:15 -0600

    actually discord starter code

---
+++ owen.py
@@ -0,0 +1,19 @@
+import bot_token
+
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('do you listen to death grips?')
+
+client.run(bot_token.token)
